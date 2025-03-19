a6a42d407b4ef5c21ea5e4562d8554b86e1d7ede
Felix Bruns <felix@bruns.hamburg>
2025-03-19 20:03:02 +0100

    first steps to generate openapi docs

---
+++ lib/api/api.dart
@@ -0,0 +1,11 @@
+// Openapi Generator last run: : 2025-03-19T19:59:32.113094
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+@Openapi(
+  inputSpec: InputSpec(path: 'openapi.json'),
+  generatorName: Generator.dio,
+  // outputDirectory: 'gamevault_api',
+  skipSpecValidation: true,
+  debugLogging: true,
+)
+class GvApi {}
