68351e36c30cc4167a208e07c9a0d1c92fae25ea
Felix Bruns <felix@bruns.hamburg>
2025-04-02 19:29:34 +0200

    setting placeholders

--- lib/widgets/settings/app_settings.dart
+++ lib/widgets/settings/app_settings.dart
@@ -5,7 +5,25 @@ class AppSettings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Column(
+      children: [
+        Text("Launch on boot"),
+        Text("Minimize ?"),
+        Text("Theme - light, dark, oled"),
+        Text("DownloadDir"),
+        //
+        // Server
+        Text("clear Cache images"),
+        Text("clear cache offline"),
+        // download and install
+        Text("Download bandwidth"),
+        Text("Extraction auto extract"),
+        Text("Extraction auto password"),
+        Text("Mount ISO instead of extraction"),
+        Text("Auto install portables"),
+        Text("Auto delete portable games install files"),
+      ],
+    );
   }
 }
 
