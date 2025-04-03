2f3f14abbed12c19344538c2f607458ad94be033
Felix Bruns <felix@bruns.hamburg>
2025-04-03 23:04:31 +0200

    enabled android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.felixbruns.clavis"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.0.13004108"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
