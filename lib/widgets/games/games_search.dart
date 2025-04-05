26c4b65fdd67d446d1ba83ba01d7697a007a55ba
Felix Bruns <felix@bruns.hamburg>
2025-04-05 16:43:43 +0200

    collapsible search bar

--- lib/widgets/games/games_search.dart
+++ lib/widgets/games/games_search.dart
@@ -16,30 +16,53 @@ class _GamesSearchState extends State<GamesSearch> {
   Widget build(BuildContext context) {
     return BlocBuilder<SearchBloc, SearchState>(
       builder: (context, state) {
-        if(!state.open) {
-          return IconButton(onPressed: () => context.read<SearchBloc>().add(SearchOpenedEvent()), icon: Icon(Icons.search));
-        }
-        return SearchAnchor(
-          builder: (context, controller) {
-            return SearchBar(
-              controller: textCtrl,
-              leading: Icon(Icons.search),
-              onChanged: (value) => context.read<SearchBloc>().add(SearchChangedEvent(text: textCtrl.text)),
-              trailing: [
-                IconButton(
-                  onPressed: () {
-                    setState(() => textCtrl.value = TextEditingValue.empty);
-                    context.read<SearchBloc>().add(SearchClosedEvent());
-                  },
-                  icon: Icon(Icons.clear),
-                ),
-              ],
-            );
-          },
-          suggestionsBuilder: (context, controller) {
-            return [];
-          },
+        Widget search = IconButton(
+          onPressed: () => context.read<SearchBloc>().add(SearchOpenedEvent()),
+          icon: Icon(Icons.search),
         );
+        if (state.open) {
+          search = ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width,
+            ),
+            child: SearchAnchor(
+              builder: (context, controller) {
+                return SearchBar(
+                  controller: textCtrl,
+                  leading: Icon(Icons.search),
+                  onChanged:
+                      (value) => context.read<SearchBloc>().add(
+                        SearchChangedEvent(text: textCtrl.text),
+                      ),
+                  trailing: [
+                    IconButton(
+                      onPressed: () {
+                        setState(() => textCtrl.value = TextEditingValue.empty);
+                        context.read<SearchBloc>().add(SearchClosedEvent());
+                      },
+                      icon: Icon(Icons.clear),
+                    ),
+                  ],
+                );
+              },
+              suggestionsBuilder: (context, controller) {
+                return [];
+              },
+            ),
+          );
+        }
+
+        return AnimatedSwitcher(
+          duration: Duration(milliseconds: 100),
+          transitionBuilder:
+              (child, animation) => ScaleTransition(
+                scale: animation,
+                alignment: Alignment.centerRight,
+                child: child,
+              ),
+          child: search,
+        );
+        
       },
     );
   }
