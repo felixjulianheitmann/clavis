943679774f4af69991a2c47edf00a18b9821e56b
Felix Bruns <felix@bruns.hamburg>
2025-04-29 20:55:29 +0200

    guard against null version.build

--- version/parse_tag.py
+++ version/parse_tag.py
@@ -7,25 +7,25 @@ if len(sys.argv) < 2:
     print(f'provided: {" ".join(sys.argv)}')
     exit(1)
 
-
 ver = semver.Version.parse(sys.argv[1][1:])
 distance="0"
 if(len(sys.argv) >= 3):
     distance = sys.argv[2]
 
 buildFlag = "0"
-if(ver.build.startswith("rc")):
-    buildFlag = "1"
-elif(ver.build.startswith("beta")):
-    buildFlag = "2"
-elif(ver.build.startswith("alpha")):
-    buildFlag = "3"
-elif(len(ver.build) > 0):
-    buildFlag = "9"
-
 buildRelease = "000"
-if(len(ver.build) > 0):
-    buildRelease = f"{ver.build.split(".")[1]:0>3}"
+if ver.build is not None:
+    if ver.build.startswith("rc"):
+        buildFlag = "1"
+    elif ver.build.startswith("beta"):
+        buildFlag = "2"
+    elif ver.build.startswith("alpha"):
+        buildFlag = "3"
+    elif len(ver.build) > 0:
+        buildFlag = "9"
+
+    if len(ver.build) > 0:
+        buildRelease = f"{ver.build.split(".")[1]:0>3}"
 
 buildDirty = "1" if int(distance) > 0 else "0"
 
